f1e327e33b769ce1f85dce570e9cb2b5cec7ae64
proider <162008145+proider@users.noreply.github.com>
2025-11-18 11:18:28 +0800

    ADD 16bit Register Functions

--- Software_I2C.h
+++ Software_I2C.h
@@ -1,52 +1,41 @@
 //
-// Created by proider on 25-11-2.
+// Created by proider on 25-11-15.
 //
 
-#ifndef SOFTWARE_I2C_H
-#define SOFTWARE_I2C_H
+#ifndef SCCB_H
+#define SCCB_H
+
+#include "gpio.h"
+#include "tim.h"
+
+#define OV2640_DEVICE_ADDRESS     0x60    // OV2640地址
+#define OV5640_DEVICE_ADDRESS     0X78		// OV5640地址
+
+#define ACK_OK  	1  			// 响应正常
+#define ACK_ERR 	0				// 响应错误
+
+#define SCCB_DEVICE_ADDR 0x21
+/* SCCB接口读写通讯地址bit0 */
+#define SCCB_WRITE_ADDRESS  0x42
+#define SCCB_READ_ADDRESS   0x43
 
 
 
+#define SCCB_CLK_PIN GPIO_PIN_8
+#define SCCB_SDA_PIN GPIO_PIN_9
 
-#define SDA_GPIO_PORT GPIOF
-#define SCL_GPIO_PORT GPIOB
+#define SCCB_CLK_PORT GPIOI
+#define SCCB_SDA_PORT GPIOI
 
-#define SDA_PIN GPIO_PIN_11
-#define SCL_PIN GPIO_PIN_0
+#define SCCB_SDA(x)  do{x?HAL_GPIO_WritePin(SCCB_SDA_PORT,SCCB_SDA_PIN,GPIO_PIN_SET):HAL_GPIO_WritePin(SCCB_SDA_PORT,SCCB_SDA_PIN,GPIO_PIN_RESET);}while(0)
+#define SCCB_CLK(x)  do{x?HAL_GPIO_WritePin(SCCB_CLK_PORT,SCCB_CLK_PIN,GPIO_PIN_SET):HAL_GPIO_WritePin(SCCB_CLK_PORT,SCCB_CLK_PIN,GPIO_PIN_RESET);}while(0)
 
+#define SCCB_READ_SDA HAL_GPIO_ReadPin(SCCB_SDA_PORT,SCCB_SDA_PIN)
 
-/* IO操作 */
-#define IIC_SCL(x)        do{ x ? \
-HAL_GPIO_WritePin(SCL_GPIO_PORT, SCL_PIN, GPIO_PIN_SET) : \
-HAL_GPIO_WritePin(SCL_GPIO_PORT, SCL_PIN, GPIO_PIN_RESET); \
-}while(0)       /* SCL */
+void sccb_init(void);
+uint8_t sccb_write_reg(uint8_t reg, uint8_t data);
+uint8_t sccb_read_reg(uint8_t reg);
+uint8_t SCCB_WriteReg_16Bit(uint16_t addr,uint8_t value);
+uint8_t SCCB_ReadReg_16Bit (uint16_t addr);
 
-#define IIC_SDA(x)        do{ x ? \
-HAL_GPIO_WritePin(SDA_GPIO_PORT, SDA_PIN, GPIO_PIN_SET) : \
-HAL_GPIO_WritePin(SDA_GPIO_PORT, SDA_PIN, GPIO_PIN_RESET); \
-}while(0)       /* SDA */
-
-#define IIC_READ_SDA     HAL_GPIO_ReadPin(SDA_GPIO_PORT, SDA_PIN) /* 读取SDA */
-#include <stdint.h>
-
-
-void Software_I2C_Init(void);
-
-void I2C_START(void);
-
-void I2C_STOP(void);
-
-void I2C_DelayUs(uint16_t us);
-
-uint8_t iic_wait_ack(void);
-
-void iic_ack(void);
-
-void iic_nack(void);
-
-void iic_send_byte(uint8_t data);
-
-uint8_t iic_read_byte(uint8_t ack);
-
-
-#endif //SOFTWARE_I2C_H
+#endif //SCCB_H
