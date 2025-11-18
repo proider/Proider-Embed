ffbd442551d2e1730f23c2492cb4d1b60bb8695d
proider <162008145+proider@users.noreply.github.com>
2025-11-18 11:17:24 +0800

    Implement 16-bit read/write functions for SCCB
    Added 16-bit read and write functions for SCCB communication.

--- SCCB.c
+++ SCCB.c
@@ -4,6 +4,8 @@
 
 #include "SCCB.h"
 
+#include "UART_Print.h"
+
 void sccb_delay() {
     __HAL_TIM_SET_COUNTER(&htim7,0);
     // 无符号减法可以正确处理计数器回绕（overflow）
@@ -76,10 +78,11 @@ uint8_t sccb_send_byte(uint8_t data) {
     SCCB_CLK(0);
 
     if (SCCB_READ_SDA) {
-        res = 1;//发送失败
+        res = ACK_ERR;//发送失败
     }
     else {
-        res = 0;//发送成功
+
+        res = ACK_OK;//发送成功
     }
     return res;
 }
@@ -146,6 +149,67 @@ void sccb_2_phase_read(uint8_t id_addr, uint8_t *dat)
     sccb_send_byte((id_addr << 1) | SCCB_READ_ADDRESS);
     *dat=sccb_read_byte();
     sccb_stop();
+}
+
+
+uint8_t SCCB_WriteHandle_16Bit (uint16_t addr)
+{
+    uint8_t status;		// 状态标志位
+
+    sccb_start();	// 启动IIC通信
+    if( sccb_send_byte(OV5640_DEVICE_ADDRESS) == ACK_OK ) //写数据指令
+    {
+        if( sccb_send_byte((uint8_t)(addr >> 8)) == ACK_OK ) //写入16位地址
+        {
+            if( sccb_send_byte((uint8_t)(addr)) != ACK_OK )
+            {
+                status = ERROR;	// 操作失败
+            }
+        }
+    }
+    status = SUCCESS;	// 操作成功
+    return status;
+}
+
+uint8_t SCCB_WriteReg_16Bit(uint16_t addr,uint8_t value)
+{
+    uint8_t status;
+
+    sccb_start(); //启动IIC通讯
+
+    if( SCCB_WriteHandle_16Bit(addr) == SUCCESS)	//写入要操作的寄存器
+    {
+        if (sccb_send_byte(value) != ACK_OK) //写数据
+        {
+            status = ERROR;
+        }
+    }
+    sccb_stop(); // 停止通讯
+
+    status = SUCCESS;	// 写入成功
+    return status;
+}
+
+uint8_t SCCB_ReadReg_16Bit (uint16_t addr)
+{
+    uint8_t value = 0;
+
+    sccb_start();		// 启动IIC通信
+
+    if( SCCB_WriteHandle_16Bit(addr) == SUCCESS) //写入要操作的寄存器
+    {
+        sccb_stop();	// 停止IIC通信
+        sccb_start(); //重新启动IIC通讯
+
+        if (sccb_send_byte(OV5640_DEVICE_ADDRESS|0X01) == ACK_OK)	// 发送读命令
+        {
+            value = sccb_read_byte();	// 读到最后一个数据时发送 非应答信号
+        }
+        sccb_stop();	// 停止IIC通信
+
+    }
+
+    return value;
 }
 
 
