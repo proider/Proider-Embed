11a146cd1b293ee491bd533afc3f015c298dd78e
proider <162008145+proider@users.noreply.github.com>
2025-11-18 11:20:40 +0800

    Update SCCB.h with new device addresses and functions
    Added support for OV2640 and OV5640 device addresses and added 16-bit register read/write functions.

--- SCCB.h
+++ SCCB.h
@@ -8,6 +8,11 @@
 #include "gpio.h"
 #include "tim.h"
 
+#define OV2640_DEVICE_ADDRESS     0x60    // OV2640地址
+#define OV5640_DEVICE_ADDRESS     0X78		// OV5640地址
+
+#define ACK_OK  	1  			// 响应正常
+#define ACK_ERR 	0				// 响应错误
 
 #define SCCB_DEVICE_ADDR 0x21
 /* SCCB接口读写通讯地址bit0 */
@@ -16,11 +21,11 @@
 
 
 
-#define SCCB_CLK_PIN GPIO_PIN_6
-#define SCCB_SDA_PIN GPIO_PIN_7
+#define SCCB_CLK_PIN GPIO_PIN_8
+#define SCCB_SDA_PIN GPIO_PIN_9
 
-#define SCCB_CLK_PORT GPIOB
-#define SCCB_SDA_PORT GPIOB
+#define SCCB_CLK_PORT GPIOI
+#define SCCB_SDA_PORT GPIOI
 
 #define SCCB_SDA(x)  do{x?HAL_GPIO_WritePin(SCCB_SDA_PORT,SCCB_SDA_PIN,GPIO_PIN_SET):HAL_GPIO_WritePin(SCCB_SDA_PORT,SCCB_SDA_PIN,GPIO_PIN_RESET);}while(0)
 #define SCCB_CLK(x)  do{x?HAL_GPIO_WritePin(SCCB_CLK_PORT,SCCB_CLK_PIN,GPIO_PIN_SET):HAL_GPIO_WritePin(SCCB_CLK_PORT,SCCB_CLK_PIN,GPIO_PIN_RESET);}while(0)
@@ -30,5 +35,8 @@
 void sccb_init(void);
 uint8_t sccb_write_reg(uint8_t reg, uint8_t data);
 uint8_t sccb_read_reg(uint8_t reg);
+uint8_t SCCB_WriteReg_16Bit(uint16_t addr,uint8_t value);
+uint8_t SCCB_ReadReg_16Bit (uint16_t addr);
 
 #endif //SCCB_H
+
